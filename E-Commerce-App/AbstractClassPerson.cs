2201b6ec87c5ad7eaffbf900e81a3d6e8b890afa
Ajay <khandekarajay427@gmail.com>
2024-12-22 11:23:01 +0530

    Added patrial functionality to Customer and Vendor class.

--- E-Commerce-App/AbstractClassPerson.cs
+++ E-Commerce-App/AbstractClassPerson.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace E_Commerce_App
 {
-    internal class AbstractClassPerson
+    internal abstract class AbstractClassPerson
     {
+        public string personName;
+        protected string email;
+        protected string phoneNumber;
+        protected int age;
+        protected string typeCustormerOrVendor;
+        protected string password;
+        public abstract string GetEmail(bool isAuthorizd);
+        public abstract string GetPhoneNumber(bool isAuthorizd);
+        public abstract int GetAge(bool isAuthorizd);
+        public abstract string GetTypeCustomerOrVendor(bool isAuthorizd);
+        public abstract void SetEmail(string newEmail ,bool isAuthorizd);
+        public abstract void SetPhoneNumber(string newPhoneNumber ,bool isAuthorizd);
+        public abstract void SetAge(int newAge ,bool isAuthorizd);
+        public abstract void SetTypeCustomerOrVendor(string newType, bool isAuthorizd);
+        public abstract void SetPassword(string newPassword ,bool isAuthorizd);
+
     }
 }
--- E-Commerce-App/Customer.cs
+++ E-Commerce-App/Customer.cs
@@ -6,7 +6,103 @@ using System.Threading.Tasks;
 
 namespace E_Commerce_App
 {
-    internal class Customer
+    internal class Customer : AbstractClassPerson
     {
+        private SortedList<string,Product> cart;
+        //private SortedList<string, Product> Orders;
+
+        public Customer(String name , string email , string phone , string password , string type , int age)
+        {
+            personName = name;
+            cart = new SortedList<string,Product>();
+            SetEmail(email, true);
+            SetPhoneNumber(phone , true);
+            SetAge(age , true);
+            SetPassword(password , true);
+            SetTypeCustomerOrVendor(type , true);
+        }
+        public override string GetEmail(bool isAuthorized)
+        {
+            return isAuthorized ? email : "Authorization required!";
+        }
+
+        public override string GetPhoneNumber(bool isAuthorized)
+        {
+            return isAuthorized ? phoneNumber : "Authorization required!";
+        }
+
+        public override int GetAge(bool isAuthorized)
+        {
+            return isAuthorized ? age : -1; // Returning -1 to indicate lack of authorization
+        }
+
+        public override string GetTypeCustomerOrVendor(bool isAuthorized)
+        {
+            return isAuthorized ? typeCustormerOrVendor : "Authorization required!";
+        }
+
+        public override void SetEmail(string newEmail, bool isAuthorized)
+        {
+            if (isAuthorized)
+            {
+                email = newEmail;
+                Console.WriteLine("Email updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Authorization required to update email.");
+            }
+        }
+
+        public override void SetPhoneNumber(string newPhoneNumber, bool isAuthorized)
+        {
+            if (isAuthorized)
+            {
+                phoneNumber = newPhoneNumber;
+                Console.WriteLine("Phone number updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Authorization required to update phone number.");
+            }
+        }
+
+        public override void SetAge(int newAge, bool isAuthorized)
+        {
+            if (isAuthorized)
+            {
+                age = newAge;
+                Console.WriteLine("Age updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Authorization required to update age.");
+            }
+        }
+
+        public override void SetTypeCustomerOrVendor(string newType, bool isAuthorized)
+        {
+            if (isAuthorized)
+            {
+                typeCustormerOrVendor = newType;
+                Console.WriteLine("Type updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Authorization required to update type.");
+            }
+        }
+        public override void SetPassword(string newPassword, bool isAuthorized)
+        {
+            if (isAuthorized)
+            {
+                password = newPassword;
+                Console.WriteLine("password updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Authorization required to update password.");
+            }
+        }
     }
 }
